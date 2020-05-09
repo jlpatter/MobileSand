da3241ff8857bdcae12c1d1d6afecf8c7ff73644
Joshua Patterson <jleegippies@gmail.com>
2020-05-08 22:15:17 -0500

    WIP Got basic sand grains working, corners of screen are broken though

--- Assets/Scripts/CubeBehavior.cs
+++ Assets/Scripts/CubeBehavior.cs
@@ -2,24 +2,14 @@
 
 public class CubeBehavior : MonoBehaviour {
     public float speed = 10.0f;
+    
+    private Rigidbody _rigidbody;
 
-    // Update is called once per frame
-    void Update() {
-        transform.position += new Vector3(Input.acceleration.x, Input.acceleration.y, 0.0f) * speed * Time.deltaTime;
-        
-        Vector3 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
-        if (viewportPos.x < 0.0) {
-            transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.0f, viewportPos.y, viewportPos.z));
-        }
-        else if (viewportPos.x > 1.0) {
-            transform.position = Camera.main.ViewportToWorldPoint(new Vector3(1.0f, viewportPos.y, viewportPos.z));;
-        }
-        
-        if (viewportPos.y < 0.0) {
-            transform.position = Camera.main.ViewportToWorldPoint(new Vector3(viewportPos.x, 0.0f, viewportPos.z));
-        }
-        else if (viewportPos.y > 1.0) {
-            transform.position = Camera.main.ViewportToWorldPoint(new Vector3(viewportPos.x, 1.0f, viewportPos.z));;
-        }
+    private void Start() {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        _rigidbody.MovePosition(transform.position + new Vector3(Input.acceleration.x, Input.acceleration.y, 0.0f) * speed * Time.fixedDeltaTime);
     }
 }
