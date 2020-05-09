c7e2b8d47accee7ef028e1f1bedb2d89afe6282d
Joshua Patterson <jleegippies@gmail.com>
2020-05-08 21:21:32 -0500

    Set screen orientation and recorded accelerometer values

---
+++ Assets/Scripts/ScreenBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenBehavior : MonoBehaviour {
+
+    public Text rotX;
+    public Text rotY;
+    public Text rotZ;
+
+    // Update is called once per frame
+    void Update() {
+        rotX.text = "Rotation X: " + Input.acceleration.x;
+        rotY.text = "Rotation Y: " + Input.acceleration.y;
+        rotZ.text = "Rotation Z: " + Input.acceleration.z;
+    }
+}
