ba91f66bf6909bb1c22b29cc9570c1e2a72674cc
Joshua Patterson <jleegippies@gmail.com>
2020-05-08 21:35:11 -0500

    Got a cube to slide around on screen

---
+++ Assets/Scripts/CubeBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour {
+    public float speed = 10.0f;
+
+    // Update is called once per frame
+    void Update() {
+        transform.position += new Vector3(Input.acceleration.x, Input.acceleration.y, 0.0f) * speed * Time.deltaTime;
+        
+        Vector3 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
+        if (viewportPos.x < 0.0) {
+            transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.0f, viewportPos.y, viewportPos.z));
+        }
+        else if (viewportPos.x > 1.0) {
+            transform.position = Camera.main.ViewportToWorldPoint(new Vector3(1.0f, viewportPos.y, viewportPos.z));;
+        }
+        
+        if (viewportPos.y < 0.0) {
+            transform.position = Camera.main.ViewportToWorldPoint(new Vector3(viewportPos.x, 0.0f, viewportPos.z));
+        }
+        else if (viewportPos.y > 1.0) {
+            transform.position = Camera.main.ViewportToWorldPoint(new Vector3(viewportPos.x, 1.0f, viewportPos.z));;
+        }
+    }
+}
