15342858b7fb5a00b29557e557f3bb3ce148e87f
Joshua Patterson <jleegippies@gmail.com>
2020-05-08 20:54:41 -0500

    Initial Commit

---
+++ Assets/Scripts/CubeBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        if (!Input.gyro.enabled) {
+            Input.gyro.enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float rotx = Input.acceleration.x;
+        float roty = Input.acceleration.y;
+        float rotz = Input.acceleration.z;
+        transform.rotation = new Quaternion(rotx, roty, rotz, 0);
+    }
+}
